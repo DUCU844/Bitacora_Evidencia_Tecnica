d547dbc063701e1b8ae3f77d521a4da822c285a6
DUCU844 <adrianducuara844@gmail.com>
2026-02-03 14:20:14 -0500

    Inicializar bitacora

---
+++ src/main/java/com/ducuara/App.java
@@ -0,0 +1,13 @@
+package com.ducuara;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
