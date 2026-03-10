9a04e828d97584ddd9a9e03fbfd354ac096fd924
DUCU844 <adrianducuara844@gmail.com>
2026-03-09 21:27:32 -0500

    Constructores y getters

--- src/main/java/dosw/bitacora/corte2/semana1/Grade.java
+++ src/main/java/dosw/bitacora/corte2/semana1/Grade.java
@@ -6,7 +6,28 @@ public class Grade {
     String subject;
     double score;
     LocalDate date;
-
     boolean passed;
 
+    public Grade(String subject, double score, LocalDate date, boolean passed) {
+        this.subject = subject;
+        this.score = score;
+        this.date = date;
+        this.passed = passed;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public boolean isPassed() {
+        return passed;
+    }
 }
--- src/main/java/dosw/bitacora/corte2/semana1/Student.java
+++ src/main/java/dosw/bitacora/corte2/semana1/Student.java
@@ -7,4 +7,27 @@ public class Student {
     String name;
     String team;
     List<Grade> grades;
+
+    public Student(String id, String name, String team, List<Grade> grades) {
+        this.id = id;
+        this.name = name;
+        this.team = team;
+        this.grades = grades;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public List<Grade> getGrades() {
+        return grades;
+    }
 }
