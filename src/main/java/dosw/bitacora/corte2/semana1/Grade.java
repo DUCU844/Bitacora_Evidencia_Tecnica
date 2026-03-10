b211ed9b9a4186144184ad2544970255e609791a
DUCU844 <adrianducuara844@gmail.com>
2026-03-09 20:57:55 -0500

    Clase Grade

---
+++ src/main/java/dosw/bitacora/corte2/semana1/Grade.java
@@ -0,0 +1,12 @@
+package dosw.bitacora.corte2.semana1;
+
+import java.time.LocalDate;
+
+public class Grade {
+    String subject;
+    double score;
+    LocalDate date;
+
+    boolean passed;
+
+}
